6f7aad61501d56fe0ce96e7f802d50c9a2233c3f
brunkovsky <brunkovsky@gmail.com>
2015-10-21 18:44:15 +0300

    alpha version 0.6 made clean main method

--- src/main/java/app/Calculator.java
+++ src/main/java/app/Calculator.java
@@ -105,6 +105,26 @@ public class Calculator {
         this.addOperation(new Calculator.Minimum(), 'i');
     }
 
+    public void runCalculator() {
+        InputElement input = new InputElement();
+        Double result = 0.0, operand1, operand2;
+
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Please enter first operand (empty string to use previous result as operand): ");
+            System.out.printf("%s%s\n", "first operand = ", operand1 = input.inputOperand(result, scanner));
+            System.out.print("Please enter second operand (empty string to use previous result as operand): ");
+            System.out.printf("%s%s\n", "second operand = ", operand2 = input.inputOperand(result, scanner));
+            System.out.print("Please enter a sign of operation: ");
+            Character signOfOperation = input.inputSign(getOperationsMap().keySet(), scanner);
+            result = calculate(operand1, operand2, signOfOperation);
+            System.out.printf("%s%s%s%s%s%s%s\n", operand1, " ", signOfOperation, " ", operand2, " = ", result);
+            System.out.print("Another operation? ");
+            if (input.inputSign(input.getAnswerSet(), scanner) == 'n')
+                break;
+        } while (true);
+    }
+
     /**
      * этот метод добавляет в мапу operationsMap ключ:знак операции и значение:сама операция
      * @param operations операция калькулятора
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,35 +1,18 @@
 package app;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.addOperation(new Calculator.Divide(), ':');          // так можно добавлять знаки для операций
-        calculator.addOperation(new Calculator.Multiply(), 'x');        // так можно добавлять знаки для операций
+
+        calculator.addOperation(new Calculator.Divide(), ':');          // так можно добавлять знаки для существующих операций
+        calculator.addOperation(new Calculator.Multiply(), 'x');        // так можно добавлять знаки для существующих операций
         calculator.addOperation(new Calculator.Operations() {
             @Override
             public double operation(double a, double b) {
                 return Math.pow(a, b);
             }
-        }, '^');// так можно добавлять операции
+        }, '^');// так можно добавлять новых операции
 
-        InputElement input = new InputElement();
-        Double result = 0.0, operand1, operand2;
-
-        do {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Please enter first operand (empty string to use previous result as operand): ");
-            System.out.printf("%s%s\n", "first operand = ", operand1 = input.inputOperand(result, scanner));
-            System.out.print("Please enter second operand (empty string to use previous result as operand): ");
-            System.out.printf("%s%s\n", "second operand = ", operand2 = input.inputOperand(result, scanner));
-            System.out.print("Please enter a sign of operation: ");
-            Character signOfOperation = input.inputSign(calculator.getOperationsMap().keySet(), scanner);
-            result = calculator.calculate(operand1, operand2, signOfOperation);
-            System.out.printf("%s%s%s%s%s%s%s\n", operand1, " ", signOfOperation, " ", operand2, " = ", result);
-            System.out.print("Another operation? ");
-            if (input.inputSign(input.getAnswerSet(), scanner) == 'n')
-                break;
-        } while (true);
+        calculator.runCalculator();
     }
 }
